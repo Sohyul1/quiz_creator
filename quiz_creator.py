eae09bced1feb41c2628789f3d6c75e0b9fdd78a
Aeron <dequitoaeron353@gmail.com>
2025-04-09 23:17:43 +0800

    made the input for the questions and the choices

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,15 @@
 # For Assignment 9: Quiz Creator
+
 # Ask for user input (question)
+questn = input("Enter the question you like: ")
+
 # Ask for user input (answer)
+a = input("Letter 'A' value: ")
+b = input("Letter 'B' value: ")
+c = input("Letter 'C' value: ")
+d = input("Letter 'D' value: ")
+
 # Make a loop to keep asking for input
+
+
 # Write the collected data into text file
