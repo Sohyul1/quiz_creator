59023617e3a518b8da93d0ebe597b9f4b3c312ae
Aeron <dequitoaeron353@gmail.com>
2025-04-09 23:27:17 +0800

    made the loop and asked the user for the right answer

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,15 +1,22 @@
 # For Assignment 9: Quiz Creator
 
-# Ask for user input (question)
-questn = input("Enter the question you like: ")
+# Start of loop
+while True:
+    # Ask for user input (question)
+    questn = input("Enter the question you like: ")
 
-# Ask for user input (answer)
-a = input("Letter 'A' value: ")
-b = input("Letter 'B' value: ")
-c = input("Letter 'C' value: ")
-d = input("Letter 'D' value: ")
+    # Ask for user input (answer)
+    a = input("Letter 'A' value: ")
+    b = input("Letter 'B' value: ")
+    c = input("Letter 'C' value: ")
+    d = input("Letter 'D' value: ")
 
-# Make a loop to keep asking for input
+    # Ask the user for the right answer
+    right_ans = input("Letter 0f the right answer: ")
+    # Make a loop to keep asking for input
+    again = input("Add another question? (yes/no): ").strip().lower()
+    if again != 'yes':
+        print("Quiz creation completed!")
+        
 
-
-# Write the collected data into text file
+    # Write the collected data into text file
