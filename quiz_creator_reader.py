91e938a67bbbaf41859122c038b47b1a9e09d1c8
Aeron <dequitoaeron353@gmail.com>
2025-04-29 22:26:55 +0800

    pseudocode

---
+++ quiz_creator_reader.py
@@ -0,0 +1,7 @@
+# For Assignment 10: Quiz
+# Open the file to read
+# Get a random question on that file 
+# Ask the user the questions
+# Reveal the right answer
+# Repeat until tehre  are o more questions in the file
+# Reveal final score
