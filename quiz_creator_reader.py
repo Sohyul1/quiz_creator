de4c93d6c1f173cdbec978081d99082d9175d44f
Aeron <dequitoaeron353@gmail.com>
2025-04-29 22:58:46 +0800

    import random then opened the file

--- quiz_creator_reader.py
+++ quiz_creator_reader.py
@@ -1,7 +1,22 @@
 # For Assignment 10: Quiz
+
+# Import random
+import random
+
 # Open the file to read
+with open("quiz_questions.txt", "r") as file:
+    lines = file.readlines()
+
 # Get a random question on that file 
+
+
 # Ask the user the questions
+
+
 # Reveal the right answer
+
+
 # Repeat until tehre  are o more questions in the file
+
+
 # Reveal final score
