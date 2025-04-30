3ca3869c410900c5dd0e010d7e502a2db34c4c98
Aeron <dequitoaeron353@gmail.com>
2025-04-30 17:21:16 +0800

    finally found the error which is in line 13, where it should be len(lines), and then i added .strip so it doesnt print the '\n'

--- quiz_creator_reader.py
+++ quiz_creator_reader.py
@@ -10,16 +10,16 @@ with open("quiz_questions.txt", "r") as file:
 # Make a list for the questions
 questions = []
 i = 0 # Line counter
-while i < len(questions):
+while i < len(lines):
     # Extract the questions and choices
     if lines[i].startswith("Question:"):
         # Extract the questions and answer line by line
-        question_text = lines[i]
-        choice_a = lines[i+1]
-        choice_b = lines[i+2]
-        choice_c = lines[i+3]
-        choice_d = lines[i+4]
-        correct_answer = lines[i+5]
+        question_text = lines[i].strip()
+        choice_a = lines[i+1].strip()
+        choice_b = lines[i+2].strip()
+        choice_c = lines[i+3].strip()
+        choice_d = lines[i+4].strip()
+        correct_answer = lines[i+5].strip()
         # Append the lines into the list
         questions.append((question_text, [choice_a, choice_b, choice_c, choice_d], correct_answer))
     i += 6
